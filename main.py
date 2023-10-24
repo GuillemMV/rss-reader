6aa6c4bbde812c0deee872e71dd976df414ca236
Guillem Magriña Vernet <gmagrinavernet@outlook.es>
2023-10-25 01:41:39 +0200

    Index page removing, css restyling

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ tiempo_url = str("https://www.aemet.es/es/noticias.rss")
 @app.route("/noticias")
 def noticias_route():
     d = feedparser.parse(noticias_url)
-    entradas = [(entry.title, entry.description, entry.published)  for entry in d.entries]
-    return render_template('main.html', entradas=entradas)
+
+    entradas = [(entry.title, entry.published)  for entry in d.entries]
+    return render_template('app.html', entradas=entradas)
 
 @app.route("/tiempo")
 def tiempo_route():
     d = feedparser.parse(tiempo_url)
-    entradas = [(entry.title, entry.description, entry.published)  for entry in d.entries]
-    return render_template('main.html', entradas=entradas)
+    entradas = [(entry.title, entry.published)  for entry in d.entries]
+    return render_template('app.html', entradas=entradas)
 
 @app.route("/")
 def index_route():
-    return render_template('index.html')
+    return render_template('app.html')
